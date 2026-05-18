45f903b8047580eb7d61eab013a4f0bc2c5aa187
Alessandro Maltese <149113559+alemaltese@users.noreply.github.com>
2026-05-18 18:02:59 +0200

    upload struttura e miglioramenti grafici

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, LOCALE_ID } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
@@ -12,7 +12,7 @@ registerLocaleData(localeIt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' })),
     provideHttpClient(withInterceptors([authInterceptor, noCacheInterceptor])),
     { provide: LOCALE_ID, useValue: 'it' }
   ]
